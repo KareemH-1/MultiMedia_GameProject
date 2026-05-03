ca8bce4276a73ade9f9df4cf75d4b5668fcd439c
KareemHassan <kareem99710@gmail.com>
2026-05-03 22:15:09 +0300

    update hero class, add range rect and draw rect , add debugging mode to show range rect , add jumping/falling , add platforms for jumpThrough and not

--- General/Form1.cs
+++ General/Form1.cs
@@ -104,89 +104,275 @@ namespace General
       
     }
 
-        
+
 
     public class Hero
     {
         public rect R = new rect();
+        public rect drawR = new rect();
+
         public float speed = 6f;
 
         public char moving = ' ';
-        public bool isJumping = false;
         public bool isRunning = false;
 
-        public int jumpHeight = 30;
+        public float velocityY = 0f;
+        public float gravity = 1.2f;
+        public float jumpForce = -18f;
+
+        public bool isGrounded = false;
+        bool wasGrounded = false;
+
+        public bool isLanding = false;
+        int landingTimer = 0;
+
+        public float max_speed = 25f; 
+        float prevBottom = 0f;
 
         public AnimationController anim = new AnimationController();
 
-        public void Draw(Graphics g)
+        public void Draw(Graphics g, bool showRanges)
         {
-            g.DrawImage(anim.playFrame(), R.x, R.y, R.width, R.height);
+            drawR.x = R.x + (R.width - drawR.width) / 2f;
+            drawR.y = R.y + (R.height - drawR.height);
+
+            g.DrawImage(anim.playFrame(), drawR.x, drawR.y, drawR.width, drawR.height);
+
+            if (showRanges)
+            {
+                Pen p = new Pen(Color.Lime, 2);
+                g.DrawRectangle(p, R.x, R.y, R.width, R.height);
+            }
         }
-        public Hero(int startX , int startY , int w , int h)
+        public Hero(int startX, int startY, int w, int h)
         {
-            R.x = startX;
-            R.y = startY;
-            R.width = w;
-            R.height = h;
+            drawR.x = startX;
+            drawR.y = startY;
+            drawR.width = w;
+            drawR.height = h;
+
+            R.width = w * 0.4f;
+            R.height = h * 0.65f;
+            R.x = startX + (w - R.width) / 2f;
+            R.y = startY + (h - R.height);
 
             createAnim();
-            anim.changeAnimation("idle" , -1);
+            anim.changeAnimation("idle", -1);
+
+            wasGrounded = true;
+            isGrounded = true;
         }
 
         void createAnim()
         {
             string[] folders = {"attack", "critical_attack", "crouch", "death",
-                "idle", "jump", "ladder_climbing", "running", "shield_defence",
-                "sliding", "spell_cast", "taking_damage", "walking", "wall_sliding" };
+            "idle", "jump_reload", "ladder_climbing", "running", "shield_defence",
+            "sliding", "spell_cast", "taking_damage", "walking", "wall_sliding",
+            "falling", "landing", "jump_flying", "jump_preparation"};
 
-            int[] numFrames = { 8, 8, 3, 12, 6, 16, 10, 8, 3, 8, 8, 4, 10, 4 };
-            // C: \Users\User\source\repos\GameProject\General\bin\Debug\Characters\Hero\Blue\attack
-            for (int i = 0; i < 14; i++)
+            int[] numFrames = { 8, 8, 3, 12, 6, 3, 10, 8, 3, 8, 8, 4, 10, 4, 4, 3, 4, 2 };
+
+            for (int i = 0; i < 18; i++)
             {
-                Animation anim = new Animation();
-                anim.name = folders[i];
-                if (folders[i] == "walking") anim.frameDelay = 0;
-                else if (folders[i] == "idle") anim.frameDelay = 2;
+                Animation a = new Animation();
+                a.name = folders[i];
+                if (folders[i] == "walking") a.frameDelay = 0;
+                else if (folders[i] == "idle") a.frameDelay = 2;
+                else if (folders[i] == "landing") a.frameDelay = 2;
                 string path = "Characters/Hero/Blue/" + folders[i] + "/";
-                for(int j =1; j <= numFrames[i]; j++)
+                for (int j = 1; j <= numFrames[i]; j++)
                 {
                     Bitmap img = new Bitmap(path + j.ToString() + ".png");
-                    anim.frames.Add(img);
+                    a.frames.Add(img);
                 }
-                this.anim.addAnim(anim);
+                this.anim.addAnim(a);
             }
-
         }
-        public void move()
+
+        public void move(List<tile> tiles)
         {
             float speedM = speed;
-            if (isRunning == true) speedM = speed * 2f;
-            if (moving == 'l') R.x -= speedM;
-            else if(moving == 'r') R.x += speedM;
+            if (isRunning) speedM = speed * 2f;
+
+            float dx = 0f;
+            if (moving == 'l') dx = -speedM;
+            else if (moving == 'r') dx = speedM;
+
+            R.x += dx;
+
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                tile t = tiles[i];
+                if (t.interact == true && t.jumpThrough == false)
+                {
+                    bool overlapping = false;
+                    if (R.x + R.width > t.R.x &&
+                        R.x < t.R.x + t.R.width &&
+                        R.y + R.height > t.R.y &&
+                        R.y < t.R.y + t.R.height)
+                    {
+                        overlapping = true;
+                    }
+
+                    if (overlapping)
+                    {
+                        if (dx > 0) R.x = t.R.x - R.width;
+                        else if (dx < 0) R.x = t.R.x + t.R.width;
+                    }
+                }
+            }
+
+            applyPhysics(tiles);
+
+            updateAnimation();
+        }
+
+        public void applyPhysics(List<tile> tiles)
+        {
+            wasGrounded = isGrounded;
+            prevBottom = R.y + R.height;
+
+            velocityY += gravity;
+            if (velocityY > max_speed) velocityY = max_speed;
+
+            R.y += velocityY;
+
+            isGrounded = false;
+
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                tile t = tiles[i];
+                if (t.interact == true)
+                {
+                    bool overlappingX = false;
+                    if (R.x + R.width > t.R.x &&
+                        R.x < t.R.x + t.R.width)
+                    {
+                        overlappingX = true;
+                    }
+
+                    if (overlappingX)
+                    {
+                        if (t.jumpThrough == true)
+                        {
+                            if (velocityY >= 0 &&
+                                prevBottom <= t.R.y &&
+                                R.y + R.height >= t.R.y)
+                            {
+                                R.y = t.R.y - R.height;
+                                velocityY = 0;
+                                isGrounded = true;
+                            }
+                        }
+                        else
+                        {
+                            bool overlappingY = false;
+                            if (R.y + R.height > t.R.y &&
+                                R.y < t.R.y + t.R.height)
+                            {
+                                overlappingY = true;
+                            }
+
+                            if (overlappingY)
+                            {
+                                if (velocityY > 0)
+                                {
+                                    R.y = t.R.y - R.height;
+                                    velocityY = 0;
+                                    isGrounded = true;
+                                }
+                                else if (velocityY < 0)
+                                {
+                                    R.y = t.R.y + t.R.height;
+                                    velocityY = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (wasGrounded == false && isGrounded == true)
+            {
+                isLanding = true;
+                landingTimer = 6;
+                anim.changeAnimation("landing", -1);
+            }
+        }
+
+
+        public void jump()
+        {
+            if (isGrounded)
+            {
+                velocityY = jumpForce;
+                isGrounded = false;
+                isLanding = false;
+                anim.changeAnimation("jump_flying", -1);
+            }
+        }
+
+        public void updateAnimation()
+        {
+            if (isLanding)
+            {
+                landingTimer--;
+                if (landingTimer <= 0 || moving != ' ' || !isGrounded)
+                {
+                    isLanding = false;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (!isGrounded)
+            {
+                if (velocityY < 0)
+                    anim.changeAnimation("jump_flying", -1);
+                else
+                    anim.changeAnimation("falling", -1);
+                return;
+            }
 
             if (moving == ' ')
-            {
                 anim.changeAnimation("idle", -1);
-            }
+            else if (isRunning)
+                anim.changeAnimation("running", -1);
             else
-            {
-                if (isRunning== true)
-                    anim.changeAnimation("running", -1);
-                else
-                    anim.changeAnimation("walking", -1);
-            }
-
+                anim.changeAnimation("walking", -1);
         }
     }
+    public class tile
+    {
+        public rect R = new rect();
+        public Color clr = Color.Black;
+        public bool interact = false;
+        public bool jumpThrough = true;
 
+        public void draw(Graphics g)
+        {
+            SolidBrush bsh = new SolidBrush(clr);
+            g.FillRectangle(bsh, R.x, R.y, R.width, R.height);
+        }
 
+        public void init(int x , int y , int width , int height)
+        {
+            R.x = x;
+            R.y = y;
+            R.height = height;
+            R.width = width;
+        }
+    }
     public partial class Form1 : Form
     {
+        bool showRanges = false;
         Bitmap off;
         Random RR = new Random();
 
         Hero hero;
+        List<tile> tiles = new List<tile>();
         Timer timer = new Timer();
 
         public Form1()
@@ -197,7 +383,7 @@ namespace General
             this.KeyDown += Form1_KeyDown;
             this.KeyUp += Form1_KeyUp;
 
-            timer.Interval = 16;
+            timer.Interval = 6;
             timer.Tick += Timer_Tick;
             timer.Start();
         }
@@ -233,16 +419,28 @@ namespace General
                 hero.moving = 'l';
 
             }
+            if (e.KeyCode == Keys.Space)
+            {
+                hero.jump();
+            }
+
             if (e.KeyCode == Keys.ShiftKey)
             {
                 hero.isRunning = true;
+            }
+
+            if (e.KeyCode == Keys.P)
+            {
+                if (showRanges == true) showRanges = false;
+                else showRanges = true;
             }
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            this.Text = hero.isRunning.ToString();
-            hero.move();
+            this.Text = hero.isRunning.ToString(); 
+            hero.move(tiles);
+
             drawDubb(this.CreateGraphics());
 
         }
@@ -252,9 +450,32 @@ namespace General
             off = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
 
 
-            hero = new Hero(30, this.ClientSize.Height - 150, 150, 150);
+            hero = new Hero(30, this.ClientSize.Height - 150 - 30, 150, 150);
             drawDubb(this.CreateGraphics());
+            initTiles();
 
+        }
+
+        void initTiles()
+        {
+            tile pnn = new tile();
+            pnn.interact = true;
+            pnn.jumpThrough = true;
+            pnn.init(0, this.ClientSize.Height - 30, this.ClientSize.Width, 30);
+            tiles.Add(pnn);
+
+            pnn = new tile();
+            pnn.interact = true;
+            pnn.jumpThrough = true;
+            pnn.init(300, this.ClientSize.Height - 150, 200, 20);
+            tiles.Add(pnn);
+
+            pnn = new tile();
+            pnn.interact = true;
+            pnn.jumpThrough = false;
+            pnn.clr = Color.DarkRed;
+            pnn.init(600, this.ClientSize.Height - 250, 200, 30);
+            tiles.Add(pnn);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -274,7 +495,11 @@ namespace General
         {
             g.Clear(this.BackColor);
 
-            hero.Draw(g);
+            hero.Draw(g , showRanges);
+            for(int i =0; i< tiles.Count; i++)
+            {
+                tiles[i].draw(g);
+            }
         }
     }
 }
